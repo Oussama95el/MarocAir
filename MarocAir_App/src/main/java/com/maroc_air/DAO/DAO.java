82d68bc2660f0e31f840d4bc5559e600a4ba5c35
Oussama95el <oussamaelbechari@gmail.com>
2022-10-12 17:06:30 +0100

    Adding DAO methods

--- MarocAir_App/src/main/java/com/maroc_air/DAO/DAO.java
+++ MarocAir_App/src/main/java/com/maroc_air/DAO/DAO.java
@@ -1,6 +1,5 @@
 package com.maroc_air.DAO;
 
-<<<<<<< HEAD
 import com.maroc_air.Connection;
 
 import java.sql.PreparedStatement;
@@ -9,31 +8,33 @@ import java.sql.SQLException;
 import java.util.HashMap;
 
 public class DAO extends Connection {
-    public static HashMap<String,DAO> repositories = new HashMap<>();
+    public static HashMap<String, DAO> repositories = new HashMap<>();
     private final String tableName;
+
     //Constructor
     public DAO(String tableName) {
         this.tableName = tableName;
     }
+
     //getters
     public String getTableName() {
         return tableName;
     }
 
-    public static DAO getRepository(String name){
+    public static DAO getRepository(String name) {
         DAO repository = repositories.get(name);
-        if (repository == null){
+        if (repository == null) {
             repository = new DAO(name);
-            repositories.put(name,repository);
+            repositories.put(name, repository);
         }
         return repository;
     }
 
-    protected String selectQuery(String[] fields){
+    protected String selectQuery(String[] fields) {
         StringBuilder query = new StringBuilder("SELECT * FROM " + getTableName() + " WHERE ");
-        for (int i=0; i<fields.length; i++){
+        for (int i = 0; i < fields.length; i++) {
             query.append(fields[i]).append(" = ?");
-            if (i < fields.length -1){
+            if (i < fields.length - 1) {
                 query.append(" AND ");
             }
         }
@@ -57,7 +58,5 @@ public class DAO extends Connection {
             return null;
         }
     }
-=======
-public class DAO {
->>>>>>> 1d291f94dcacd781e87329ea70177d493302bdb8
+
 }
