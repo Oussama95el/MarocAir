db1921589c750764650ae9e043999b91fdf8ab6f
Oussama95el <oussamaelbechari@gmail.com>
2022-10-18 10:39:13 +0100

    updates

--- MarocAir_App/src/main/java/com/maroc_air/Controller/Reservation.java
+++ MarocAir_App/src/main/java/com/maroc_air/Controller/Reservation.java
@@ -1,5 +1,5 @@
 package com.maroc_air.Controller;
-import com.maroc_air.DAO.Dao;
+import com.maroc_air.DAO.DaoManager;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -8,7 +8,7 @@ import java.io.IOException;
 @WebServlet(name ="Reservation", value = "/Reservation")
 public class Reservation extends HttpServlet {
     public Reservation() {
-        Dao ReservationDao = new Dao("reservation");
+
     }
     @Override
     protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -34,13 +34,11 @@ public class Reservation extends HttpServlet {
     }
     private void insertReservation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        com.maroc_air.Modelles.Reservation newReservation = new com.maroc_air.Modelles.Reservation(1,1,35,true);
-        Dao ReservationDao = new Dao("reservation");
-        // insert in method dao
+//         insert in method dao
         response.sendRedirect("list");
     }
     private void deleteReservation(HttpServletRequest request, HttpServletResponse response) throws ServletException , IOException{
         int id = Integer.parseInt(request.getParameter("id"));
-        Dao ReservationDao = new Dao("reservation");
         // method to delete reservation by id
         //and return response in a list affichage
         response.sendRedirect("list");
--- MarocAir_App/src/main/java/com/maroc_air/Modelles/Reservation.java
+++ MarocAir_App/src/main/java/com/maroc_air/Modelles/Reservation.java
@@ -1,25 +1,36 @@
 package com.maroc_air.Modelles;
 
 import com.maroc_air.Controller.Vol;
-import com.maroc_air.DAO.TableTest;
-
+import com.maroc_air.DAO.Column;
+import com.maroc_air.DAO.Table;
 import java.time.LocalTime;
 import java.util.Locale;
 
+@Table(tableName = "reservation")
 public class Reservation {
+    @Column
     private int nbr_adulte;
+    @Column
     private int nbr_enfant;
+    @Column
     private int prixReservation;
+    @Column
     private boolean archive;
-    private Vol vol;
+    @Column
+    private int id_vol;
+    @Column
+    private int id_client;
 
 
-    @TableTest(tableName = "reservation")
-    public Reservation(int nbr_adulte, int nbr_enfant, int prixReservation, boolean archive,Vol vol) {
+    //Constructor
+    public Reservation(int nbr_adulte, int nbr_enfant, int prixReservation, boolean archive,int id_client,int id_vol) {
         this.nbr_adulte = nbr_adulte;
         this.nbr_enfant = nbr_enfant;
         this.prixReservation = prixReservation;
         this.archive = archive;
+        this.id_vol = id_vol;
+        this.id_client = id_client;
     }
 
+    public Reservation(){}
 }
