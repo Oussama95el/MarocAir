4221370f9ce9324df8bc0390096cfd6647fcf30c
Oussama95el <oussamaelbechari@gmail.com>
2022-10-20 09:17:28 +0100

    updates

---
+++ MarocAir_App/src/main/java/com/maroc_air/Controller/login/login.java
@@ -0,0 +1,23 @@
+package com.maroc_air.Controller.login;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "login", value = "/login")
+public class login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Authentication/Login.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        response.getWriter().println(email +" | "+ password );
+    }
+}
--- MarocAir_App/src/main/webapp/Authentification/login.jsp
+++ MarocAir_App/src/main/webapp/Authentification/login.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: abdessalm staili
-  Date: 10/16/2022
-  Time: 7:40 PM
+  User: adm
+  Date: 19/10/2022
+  Time: 13:54
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<jsp:include page="components/card.jsp"/>
+
 </body>
 </html>
